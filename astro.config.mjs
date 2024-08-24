7b403cf26e432b602c8b063db937dc9773919562
alawllm <alicja.willam@gmail.com>
2024-08-24 10:47:01 +0200

    config: absolute imports, feat: image

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,11 @@ import tailwind from '@astrojs/tailwind'
 export default defineConfig({
   // ...
   integrations: [tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
+    },
+  },
 })
